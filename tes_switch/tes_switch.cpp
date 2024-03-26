abcb9300fcfffa0f3f06351356e769d9f3f80531
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-03-26 11:06:24 +0700

    memasukkan fungsi penjumlahan, pengurangan, perkalian, pambagian

--- tes_switch/tes_switch.cpp
+++ tes_switch/tes_switch.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+    return a - b;
+}
+
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+
+float pembagian(float a, float b)
+{
+    return a / b;
+}
+
 int main()
 {
 
